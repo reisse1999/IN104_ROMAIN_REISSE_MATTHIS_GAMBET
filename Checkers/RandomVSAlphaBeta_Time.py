f0ea0456e463ffd49dc3f8ffe35b0af0b6ed624c
reisse1999 <48726603+reisse1999@users.noreply.github.com>
2019-05-21 20:59:34 +0200

    Update RandomVSAlphaBeta_Time.py

--- Checkers/RandomVSAlphaBeta_Time.py
+++ Checkers/RandomVSAlphaBeta_Time.py
@@ -3,9 +3,10 @@ from MinimaxBrainAlphaBetaTime import MinimaxBrainABT
 from randomBrain import RandomBrain
 
 brain1 = RandomBrain()
-human_time = 10 #the human will have 10 secs to play
+brain1_time = 1 #the human will have 10 secs to play
 brain2 = MinimaxBrainABT(simu.game.Game.defaultConfig, simu.game.Game.defaultRules)
-game = simu.Game(brain1, human_time, brain2, brain2.time)
+brain2_time = 0.5
+game = simu.Game(brain1, brain1_time, brain2, brain2_time)
 game.displayLevel = 1   # this prints the board after each move
 game.runGame()
 print(game.pdn) #print the summary of the game.
