a60e8b215419e86533c6c84e878e86288f250cab
reisse1999 <48726603+reisse1999@users.noreply.github.com>
2019-05-22 19:04:19 +0200

    Update MinimaxBrainAlphaBetaTime.py

--- Checkers/MinimaxBrainAlphaBetaTime.py
+++ Checkers/MinimaxBrainAlphaBetaTime.py
@@ -15,24 +15,20 @@ class MinimaxBrainABT:
     def play(self, gameState, timeLimit):
         #use minimax here to return the next state with higher score
         possibleStates = gameState.findNextStates()
-        T = []
         max_T = -10000000000
         n = len(possibleStates)
         temps_T = (timeLimit)-(self.delai)
-        temps = (temps_T)/(n)
         i = 0
         for state in possibleStates:
+            temps = (temps_T)/(n-i)
             debut = time.time()
             Mini = minimax(state, temps, True, simu.GameState.findNextStates, evaluate, self.delai)
             if(Mini > max_T):
                 chosen = state
-            T.append(Mini)
-            max_T = max(T)
+                max_T = Mini
             fin = time.time()
             temps_T -= (fin-debut)
             i += 1
-            if(i != n):
-                temps = (temps_T)/(n-i)
         return(chosen)
 
 
