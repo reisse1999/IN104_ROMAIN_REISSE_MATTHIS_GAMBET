031d3b0d36b52ea498f8ff2de98b63946aad8a04
gambet666 <48726565+gambet666@users.noreply.github.com>
2019-05-22 18:54:00 +0200

    Update evaluation.py

--- Checkers/evaluation.py
+++ Checkers/evaluation.py
@@ -6,10 +6,16 @@ def evaluate (gS):
 	boardState = gS.boardState
 	nber_black = 0
 	nber_white = 0
+	nberK_black = 0
+	nberK_white = 0
 	cells = boardState.cells
 	for cell in cells :
 		if Cell.isBlack(cell) :
 			nber_black+=1
+			if Cell.isKing(cell):
+				nberK_black+=1
 		if Cell.isWhite(cell):
 			nber_white+=1
-	return (nber_white - nber_black)
+			if Cell.isKing(cell):
+				nberK_white+=1
+	return (nber_white - nber_black + 2*(nberK_white-nberK_black))   #une dame compte pour 3 pions (coefficient deux car les dames sont comptées aussi pour des pions)
